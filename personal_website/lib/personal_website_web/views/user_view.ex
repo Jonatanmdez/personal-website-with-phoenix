1e268812753114de84e92d9e531986b51674b5be
Jonatan <jonatanmenendez@gmail.com>
2019-05-28 20:06:55 +0200

    Create migration for User

---
+++ personal_website/lib/personal_website_web/views/user_view.ex
@@ -0,0 +1,3 @@
+defmodule PersonalWebsiteWeb.UserView do
+  use PersonalWebsiteWeb, :view
+end
---
+++ personal_website/priv/repo/migrations/20190528180625_create_users.exs
@@ -0,0 +1,14 @@
+defmodule PersonalWebsite.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :email, :string
+      add :username, :string
+      add :encrypted_password, :string
+
+      timestamps()
+    end
+
+  end
+end
